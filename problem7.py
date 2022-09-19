03abdf8b8d1261aa219248b5a56b53dee33db6f6
WayneMusungu <waynemuyera17@gmail.com>
2022-09-19 13:44:11 +0300

    Problem7

--- problem7.py
+++ problem7.py
@@ -6,3 +6,18 @@ we can see that the 6th prime is 13.
 What is the 10 001st prime number?
 
 """
+def is_prime(number):
+  r = int(number**0.5)
+  for x in range(2,r+1):
+    if number % x == 0:
+      return False
+  return True
+
+primelist = []
+num = 1
+while len(primelist) < 10001:
+  num += 1
+  if is_prime(num):
+    primelist.append(num)
+  
+print(primelist[-1])
