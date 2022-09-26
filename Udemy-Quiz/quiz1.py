98a00fd5a0694a13823fa892275923f9b2d38396
WayneMusungu <waynemuyera17@gmail.com>
2022-09-26 05:47:47 +0300

    quiz1 recursive function

--- Udemy-Quiz/quiz1.py
+++ Udemy-Quiz/quiz1.py
@@ -9,5 +9,17 @@ def sumofDigit(digit):
 digit = 8976
 print(sumofDigit(digit))
 
+#Use recursive function
+def sumofDigit(digit):
+#Use assert keyword to find sum of positive integers numbers only
+	assert digit >=0 and int(digit) == digit, "The number must be a positive integer only"
+	if digit == 0:
+		return 0
+	else:
+#Divide the digit with 10 and taking the remainder and sum it up with the next
+#value of our next function f(digit)=digit%10+f(digit//10) which will be called recursively
+#based on digit divided by 10
+		return int(digit%10) + sumofDigit(digit//10)
+print(sumofDigit(8976))
 
 
