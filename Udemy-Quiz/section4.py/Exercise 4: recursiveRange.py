9202ad412b544f775a360a7d4779c2c7aa0aee6f
WayneMusungu <waynemuyera17@gmail.com>
2022-09-28 14:02:06 +0300

    Use iterative approach in Exercise 4

--- Udemy-Quiz/section4.py/Exercise 4: recursiveRange.py
+++ Udemy-Quiz/section4.py/Exercise 4: recursiveRange.py
@@ -1,7 +1,7 @@
 #Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
 
 #Examples
-# 1. recursiveRange(6) # 2
+# 1. recursiveRange(6) # 21
 # 2. recursiveRange(10) # 55
 
 def recursiveRange(num):
@@ -11,3 +11,12 @@ def recursiveRange(num):
 		return num + recursiveRange(num-1)
 num = 10
 print(recursiveRange(num))
+
+# Use iterative Approach
+def iterativeRange(num):
+	total = 0
+	for i in range(1,num+1):
+		total += i
+	return total
+num = 6
+print(iterativeRange(num))
