c6e7810c09fe52f62f927b8eb91f31d6369c1a5a
WayneMusungu <waynemuyera17@gmail.com>
2022-09-30 15:44:13 +0300

    Hacker rank strings

---
+++ Udemy-Quiz/section4.py/Exercise 6:Reverse.py
@@ -0,0 +1,13 @@
+#Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+#Examples
+
+#reverse('python') # 'nohtyp'
+#reverse('appmillers') # 'srellimppa'
+
+def reverse(strng):
+	return strng[::-1]
+strng="python"
+print(reverse(strng))
+
+
