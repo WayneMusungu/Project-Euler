dc5eeb344c0bcf91c4dd3c1c6aa9691e0c380b62
WayneMusungu <waynemuyera17@gmail.com>
2022-10-06 11:13:29 +0300

    Bubble Sort

---
+++ Algorithm/Bubble-Sort.py
@@ -0,0 +1,10 @@
+lst = [2,10,13,28,29,5,4,3,1]
+def bubble_sort(lst):
+	for i in range(0, len(lst)):
+		for j in range (0, len(lst)):
+			if lst[i] < lst[j]:
+				lst[i], lst[j] = lst[j], lst[i]
+	return lst
+print(bubble_sort(lst))
+
+
