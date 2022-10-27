f165abce91248ba2f91029fbfcdc78238550581c
WayneMusungu <waynemuyera17@gmail.com>
2022-10-27 11:19:04 +0300

    Bubble Sort

--- Algorithm/Bubble-Sort.py
+++ Algorithm/Bubble-Sort.py
@@ -1,4 +1,6 @@
-#This is an algorithm that compares twp adjacent elements until they are in intended order
+# This is an algorithm that compares twp adjacent elements until they are in intended order
+# Time complexity quadratic because of nested loops
+# Space complexity linear, no extra space is required
 lst = [2,10,13,28,29,5,4,3,1]
 def bubble_sort(lst):
 	for i in range(0, len(lst)):
@@ -8,4 +10,11 @@ def bubble_sort(lst):
 	return lst
 print(bubble_sort(lst))
 
+# When to use Bubble Sort
+	# When the input is already sorted
+	# Space is a concern
+	# Easy to implement
+
+# When to avaoid Bubble Sort
+	# Average time complexity is poor
 
