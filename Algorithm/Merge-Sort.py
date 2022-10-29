9660953138baf241a6fe58f3f2700574be459ef7
WayneMusungu <waynemuyera17@gmail.com>
2022-10-29 20:11:20 +0300

    Merge Sort

---
+++ Algorithm/Merge-Sort.py
@@ -0,0 +1,5 @@
+# Merge Sort
+
+# This is a divide and conquer algorithm
+# Divide the input array into two halves and keep halving recursively until they become too small that cannot be broken further
+# Merge halves by sorting them
