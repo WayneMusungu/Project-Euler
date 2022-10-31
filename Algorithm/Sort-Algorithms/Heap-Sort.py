b352b7b878b3d3968b41bb9273e75d1d7eece730
WayneMusungu <waynemuyera17@gmail.com>
2022-10-31 12:03:05 +0300

    Sorting Algorithms

---
+++ Algorithm/Sort-Algorithms/Heap-Sort.py
@@ -0,0 +1,10 @@
+# Heap Sort
+
+#This sorting algoritm compromises of two steps:
+ #1. Insert data to Binary Heap
+ #2. Extract data from Binary Heap
+# It is best suited with array, it doesn't work with Linked List
+
+# Binary Heap is a binary tree with special properties
+	# The value of any given node must be less or equal of it children(min heap)
+	# The value of any given node must be greater or equal of its children(max heap)
