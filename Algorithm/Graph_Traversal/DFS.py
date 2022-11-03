931372c8d4e69a643e4f771ed60cd03b77e28443
WayneMusungu <waynemuyera17@gmail.com>
2022-11-03 07:55:59 +0300

    Depth First Search

---
+++ Algorithm/Graph_Traversal/DFS.py
@@ -0,0 +1,47 @@
+# Depth First Search
+#This is an algorithm used for traversing graphs or trees
+
+# The algorithm starts at the root node (select some arbitrary node) 
+# and explore as far as possible along each branch before backtracking
+
+# Time Complexity O(V+E)
+# Space Complexity O(V+E)
+
+
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+    
+    def addEdge(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+
+    
+    def dfs(self, vertex):
+        visited = [vertex]
+        stack = [vertex]
+        while stack:
+            popVertex = stack.pop()
+            print(popVertex)
+            for adjacentVertex in self.gdict[popVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    stack.append(adjacentVertex)
+    
+
+
+
+customDict = { "a" : ["b","c"],
+            "b" : ["a", "d", "e"],
+            "c" : ["a", "e"],
+            "d" : ["b", "e", "f"],
+            "e" : ["d", "f", "c"],
+            "f" : ["d", "e"]
+               }
+
+
+
+g = Graph(customDict)
+g.dfs("a")
+
